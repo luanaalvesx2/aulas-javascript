bc0aeeafc939217abfc0bd62befd20f1fab1b31d
luanaalvesx2 <luana4208@gmail.com>
2025-08-06 14:33:51 -0300

    Array com dados diversos/interogeneos

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -13,4 +13,11 @@ console.log(vocalistas[0]);
 console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
 
 
+// Relembrando como criar/usar arrays(vetor)//
+//No JS você pode colocar qualquer coisa em um Array//
+let seila = 6;
+const coisas = [10, "Senac", "<h2>Oie<h2>", seila, 15.88];
+console.log(coisas[1]);
+console.log(coisas[4]);
+
 
