6db8ba3051136d0e10da5b99c39abd3d6966a545
luanaalvesx2 <luana4208@gmail.com>
2025-08-19 14:36:26 -0300

    condicional usando apenas o if (SE)

---
+++ js/05-condicionais.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*Comandos Condicionais mais comuns: 
+if   -> SE
+else -> SENÃO 
+*/
+
+console.log("Exemplo 1");
+
+let numero = 50;
+
+/*Se o numero for maior ou igual a 10
+condicional simples(usa apenas o if)*/
+if(numero >= 10){
+    console.log("Se você está vendo essa mensagem é porque a condição e VERDADEIRA!");
+}
+
+
